e487facf79e4898ff1df00c017b3a39965b98977
Yexi Jiang <yexijiang@gmail.com>
2013-01-17 13:31:23 -0500

    Implementing BFPRT

--- src/main/java/edu/fiu/yxjiang/stream/util/BFPRT.java
+++ src/main/java/edu/fiu/yxjiang/stream/util/BFPRT.java
@@ -1,5 +1,6 @@
 package edu.fiu.yxjiang.stream.util;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import backtype.storm.tuple.Tuple;
@@ -11,12 +12,64 @@ import backtype.storm.tuple.Tuple;
  */
 public class BFPRT {
 	
-	public static Tuple bfprt(List<Tuple> tupleList, int k) {
+	/**
+	 * Find the ith-smallest elements in tupleList in O(n).
+	 * @param tupleList
+	 * @param i
+	 * @return
+	 */
+	public static Tuple bfprt(List<Tuple> tupleList, int i) {
 		
+		List<TupleWrapper> tupleWrapperList = new ArrayList<TupleWrapper>();
+		for(Tuple tuple : tupleList) {
+			tupleWrapperList.add(new TupleWrapper(tuple.getLong(1), tuple));
+		}
 		
-		
-		return null;
+		return bfprtWrapper(tupleWrapperList, i, 0, tupleWrapperList.size()).tuple;
 	}
+	
+	public static TupleWrapper bfprtWrapper(List<TupleWrapper> tupleWrapperList, int i, int left, int right) {
+		
+		if(left == right) {
+			return tupleWrapperList.get(left);
+		}
+		
+		int q = partitionSingleSide(tupleWrapperList, left, right);
+		
+		int k = q - left + 1; 
+		if(i > k) {	//	recursively find right part
+			return bfprtWrapper(tupleWrapperList, q + 1, right, i);
+		}
+		else if (i < k) {	//	recursively find left part
+			return bfprtWrapper(tupleWrapperList, left, q - 1, i - k);
+		}
+		else {
+			return tupleWrapperList.get(q);
+		}
+	}
+	
+	/**
+	 * Partition single side version.
+	 * @param tupleWrapperList
+	 * @param left
+	 * @param right
+	 * @return
+	 */
+	public static int partitionSingleSide(List<TupleWrapper> tupleWrapperList, int left, int right) {
+		int pivotIdx = right;
+		TupleWrapper pivot = tupleWrapperList.get(pivotIdx);
+		int bar = left - 1;
+		
+		for(int i = left; i < right; ++i) {
+			if(tupleWrapperList.get(i).compareTo(pivot) < 0) {
+				++bar;
+				swap(tupleWrapperList, bar, i);
+			}
+		}
+		swap(tupleWrapperList, bar + 1, pivotIdx);
+		return bar + 1;
+	}
+	
 	
 	/**
 	 * Sort the group-of-5 with insertionSort.
@@ -24,7 +77,56 @@ public class BFPRT {
 	 * @param left
 	 * @param right
 	 */
-	private static void insertionSort(List<Tuple> tupleList, int left, int right) {
+	public static void insertionSort(List<TupleWrapper> tupleWrapperList, int left, int right) {
+		for(int i = left + 1; i < right; ++i) {
+			int iHole = i;
+			TupleWrapper wrapper = tupleWrapperList.get(iHole);
+			while(iHole > 0 && tupleWrapperList.get(iHole - 1).compareTo(wrapper) > 0) {
+				tupleWrapperList.set(iHole - 1, wrapper);
+				--iHole;
+			}
+			tupleWrapperList.set(iHole, wrapper);
+		}
+	}
+	
+	private static void swap(List<TupleWrapper> tupleWrapperList, int left, int right) {
+		TupleWrapper tmp = tupleWrapperList.get(left);
+		tupleWrapperList.set(left, tupleWrapperList.get(right));
+		tupleWrapperList.set(right, tmp);
+	}
+	
+	/**
+	 * The wrapper that make the BFPRT algorithm more generic.
+	 * @author yexijiang
+	 *
+	 */
+	public static class TupleWrapper implements Comparable {
+		
+		double score;
+		Tuple tuple;
+		
+		public TupleWrapper(double score, Tuple tuple) {
+			super();
+			this.score = score;
+			this.tuple = tuple;
+		}
+
+		@Override
+		public int compareTo(Object o) {
+			if(o instanceof TupleWrapper) {
+				TupleWrapper wrapper = (TupleWrapper)o;
+				if(score == wrapper.score) {
+					return 0;
+				}
+				else if(score > wrapper.score) { 
+					return 1;
+				}
+				else {
+					return -1;
+				}
+			}
+			return 0;
+		}
 		
 	}
 	
--- src/test/java/edu/fiu/yxjiang/stream/util/TestBFPRT.java
+++ src/test/java/edu/fiu/yxjiang/stream/util/TestBFPRT.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Random;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import edu.fiu.yxjiang.stream.util.BFPRT.TupleWrapper;
@@ -19,12 +20,22 @@ public class TestBFPRT {
 	public void before() {
 		Random rnd = new Random();
 		wrapperList = new ArrayList<TupleWrapper>();
-		for(int i = 0; i < 18; ++i) {
-			TupleWrapper wrapper = new TupleWrapper(null, rnd.nextInt(5000));
+		for(int i = 0; i < 5; ++i) {
+			TupleWrapper wrapper = new TupleWrapper(rnd.nextInt(500), null);
 			wrapperList.add(wrapper);
+			System.out.print(wrapper.score + "\t");
 		}
+		System.out.println();
+//		wrapperList.add(new TupleWrapper(2111.0, null));
+//		wrapperList.add(new TupleWrapper(33.0, null));
+//		wrapperList.add(new TupleWrapper(39.0, null));
+//		wrapperList.add(new TupleWrapper(15.0, null));
+//		wrapperList.add(new TupleWrapper(11.0, null));
+//		wrapperList.add(new TupleWrapper(11.0, null));
+//		wrapperList.add(new TupleWrapper(5.0, null));
 	}
 	
+	@Ignore
 	@Test
 	public void testInsertionSort() {
 		BFPRT.insertionSort(wrapperList, 0, wrapperList.size());
@@ -33,4 +44,45 @@ public class TestBFPRT {
 			assertTrue(previous - wrapper.score <= 0);
 		}
 	}
+	
+	@Ignore
+	@Test
+	public void testPartition() {
+		int k = BFPRT.partitionSingleSide(wrapperList, 0, wrapperList.size() - 1);
+		TupleWrapper pivotWrapper = wrapperList.get(k);
+		
+		for(int i = 0; i < wrapperList.size(); ++i) {
+			if(i == k) {
+				System.out.print("[");
+			}
+			System.out.print(wrapperList.get(i).score);
+			if(i == k) {
+				System.out.print("]");
+			}
+			System.out.print("\t");
+		}
+		System.out.println();
+		
+		for(int i = 0; i < k; ++i) {
+			assertTrue(pivotWrapper.compareTo(wrapperList.get(i)) >= 0);
+		}
+		for(int i = k + 1; i < wrapperList.size(); ++i) {
+			assertTrue(pivotWrapper.compareTo(wrapperList.get(i)) <= 0);
+		}
+	}
+	
+//	@Ignore
+	@Test
+	public void testBfprtWrapper() {
+		int k = (int)Math.round(wrapperList.size() / 2 + 0.5);
+		TupleWrapper median = BFPRT.bfprtWrapper(wrapperList, k, 0, wrapperList.size() - 1);
+		
+		for(int i = 0; i < wrapperList.size(); ++i) {
+			System.out.print(wrapperList.get(i).score + "\t");
+		}
+		System.out.println();
+		
+		System.out.println("Median:" + median.score);
+	}
+	
 }
